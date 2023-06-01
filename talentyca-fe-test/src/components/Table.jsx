f5e4363295e28ae5d6921f48bc3ae741cc769dd0
Helmi <helmiariefm@gmail.com>
2023-06-01 23:23:04 +0700

    everything is done, just need to review it again

--- talentyca-fe-test/src/components/Table.jsx
+++ talentyca-fe-test/src/components/Table.jsx
@@ -1,61 +1,115 @@
+import { useState } from 'react';
 import '../styles/table.css';
+import mahasiswa from '../mahasiswa.json';
+import foto from '../assets/userIcon.png';
 
 const Table = () => {
-  const options = [1,2,3,4,5,6,7,8,9,10]
+  const options = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  const [aspekPenilaian, setAspekPenilaian] = useState({
+    aspek_penilaian_1: {},
+    aspek_penilaian_2: {},
+    aspek_penilaian_3: {},
+    aspek_penilaian_4: {},
+  });
+
+  const handleSimpan = (e) => {
+    e.preventDefault();
+    const nilaiMahasiswa = {
+      aspek_penilaian_1: {},
+      aspek_penilaian_2: {},
+      aspek_penilaian_3: {},
+      aspek_penilaian_4: {},
+    };
+
+    mahasiswa.forEach((mhs, index) => {
+      const namaMahasiswa = mhs.nama;
+      nilaiMahasiswa.aspek_penilaian_1[namaMahasiswa] = parseInt(
+        e.target.elements[`aspek_penilaian_1_${index}`].value
+      );
+      nilaiMahasiswa.aspek_penilaian_2[namaMahasiswa] = parseInt(
+        e.target.elements[`aspek_penilaian_2_${index}`].value
+      );
+      nilaiMahasiswa.aspek_penilaian_3[namaMahasiswa] = parseInt(
+        e.target.elements[`aspek_penilaian_3_${index}`].value
+      );
+      nilaiMahasiswa.aspek_penilaian_4[namaMahasiswa] = parseInt(
+        e.target.elements[`aspek_penilaian_4_${index}`].value
+      );
+    });
+
+    setAspekPenilaian(nilaiMahasiswa);
+  };
 
   return (
-    <>
-        <div className='table-container'>
-            <table className="table">
-                <thead>
-                    <tr>
-                    <th>Foto</th>
-                    <th>Mahasiswa</th>
-                    <th>Aspek Penilaian 1</th>
-                    <th>Aspek Penilaian 2</th>
-                    <th>Aspek Penilaian 3</th>
-                    <th>Aspek Penilaian 4</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                    <td><img className="profile-picture" src="profile.jpg" alt="Profile" /></td>
-                    <td>John Doe</td>
-                    <td>
-                        <select>
-                        {options.map((option) => (
-                            <option key={option} value={option}>{option}</option>
-                        ))}
-                        </select>
-                    </td>
-                    <td>
-                        <select>
-                        {options.map((option) => (
-                            <option key={option} value={option}>{option}</option>
-                        ))}
-                        </select>
-                    </td>
-                    <td>
-                        <select>
-                        {options.map((option) => (
-                            <option key={option} value={option}>{option}</option>
-                        ))}
-                        </select>
-                    </td>
-                    <td>
-                        <select>
-                        {options.map((option) => (
-                            <option key={option} value={option}>{option}</option>
-                        ))}
-                        </select>
-                    </td>
-                    </tr>
-                </tbody>
-            </table>
-            
+    <div style={{ width: '100%', marginBottom: '10%' }}>
+      <form onSubmit={handleSimpan}>
+        <div className="table-container">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Foto</th>
+                <th>Mahasiswa</th>
+                <th>Aspek Penilaian 1</th>
+                <th>Aspek Penilaian 2</th>
+                <th>Aspek Penilaian 3</th>
+                <th>Aspek Penilaian 4</th>
+              </tr>
+            </thead>
+            <tbody>
+              {mahasiswa.map((mhs, index) => (
+                <tr key={mhs.nama}>
+                  <td>
+                    <img className="profile-picture" src={foto} alt="Profile" />
+                  </td>
+                  <td>{mhs.nama}</td>
+                  <td>
+                    <select name={`aspek_penilaian_1_${index}`}>
+                      {options.map((option) => (
+                        <option key={option} value={option}>
+                          {option}
+                        </option>
+                      ))}
+                    </select>
+                  </td>
+                  <td>
+                    <select name={`aspek_penilaian_2_${index}`}>
+                      {options.map((option) => (
+                        <option key={option} value={option}>
+                          {option}
+                        </option>
+                      ))}
+                    </select>
+                  </td>
+                  <td>
+                    <select name={`aspek_penilaian_3_${index}`}>
+                      {options.map((option) => (
+                        <option key={option} value={option}>
+                          {option}
+                        </option>
+                      ))}
+                    </select>
+                  </td>
+                  <td>
+                    <select name={`aspek_penilaian_4_${index}`}>
+                      {options.map((option) => (
+                        <option key={option} value={option}>
+                          {option}
+                        </option>
+                      ))}
+                    </select>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-        <button className="custom-button">Submit</button>            
-    </>
+        <button type="submit" className="custom-button">
+          Simpan
+        </button>
+      </form>      
+      <pre style={{marginTop: '6rem', marginLeft: '14rem', marginRight: '14rem'}}>{JSON.stringify(aspekPenilaian, null, 2)}</pre>
+    </div>
   );
 };
 
